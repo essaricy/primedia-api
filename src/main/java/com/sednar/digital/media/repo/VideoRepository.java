d75a5d066511bde381053d80dc6622535f7bc137
Srikanth Ragi <srikanthkumar.ragi@gmail.com>
2021-06-27 00:29:19 +0530

    Added API for video thumbnail generation

--- src/main/java/com/sednar/digital/media/repo/VideoRepository.java
+++ src/main/java/com/sednar/digital/media/repo/VideoRepository.java
@@ -6,8 +6,12 @@ import com.sednar.digital.media.repo.selectors.ThumbnailColumn;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VideoRepository extends CrudRepository<Video, Long> {
+
+    List<Video> findAll();
 
     ThumbnailColumn getById(Long id);
 
--- src/main/java/com/sednar/digital/media/service/ContentService.java
+++ src/main/java/com/sednar/digital/media/service/ContentService.java
@@ -5,17 +5,19 @@ import com.sednar.digital.media.common.type.Type;
 import com.sednar.digital.media.repo.ImageRepository;
 import com.sednar.digital.media.repo.VideoRepository;
 import com.sednar.digital.media.repo.entity.Image;
+import com.sednar.digital.media.repo.entity.Video;
 import com.sednar.digital.media.repo.selectors.ContentColumn;
 import com.sednar.digital.media.repo.selectors.ThumbnailColumn;
 import com.sednar.digital.media.resource.v1.model.BatchDto;
 import com.sednar.digital.media.service.content.image.ImageContentProcessor;
+import com.sednar.digital.media.service.content.video.VideoContentProcessor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
-import java.io.IOException;
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -30,6 +32,9 @@ public class ContentService {
 
     @Autowired
     ImageContentProcessor imageContentProcessor;
+
+    @Autowired
+    VideoContentProcessor videoContentProcessor;
 
     @Autowired
     public ContentService(ImageRepository imageRepository,
@@ -69,16 +74,34 @@ public class ContentService {
             images.stream().forEach(image -> {
                 Long id = image.getId();
                 try {
-                    File file = FileSystem.save(sessionId + "_" + id + ".jpg", image.getContent());
+                    File file = FileSystem.save(getFileName(sessionId, id, "jpg"), image.getContent());
                     File thumbnail = imageContentProcessor.generateThumbnail(file);
                     image.setThumbnail(FileUtils.readFileToByteArray(thumbnail));
                     log.info("Generated thumbnail for the id={}", id);
-                    FileUtils.deleteQuietly(file);
-                    FileUtils.deleteQuietly(thumbnail);
+                    deleteQuietly(file, thumbnail);
                     imageRepository.save(image);
                     log.info("Saved thumbnail for the id={}", id);
                     successList.add(id);
-                } catch (IOException e) {
+                } catch (Exception e) {
+                    log.error("Unable to save the file with image id {}", id);
+                    failureList.add(id);
+                }
+            });
+        } else if (type == Type.VIDEO) {
+            List<Video> videos = videoRepository.findAll();
+            log.info("There are {} videos found for generating thumbnails", videos.size());
+            videos.stream().forEach(video -> {
+                Long id = video.getId();
+                try {
+                    File file = FileSystem.save(getFileName(sessionId, id, "mp4"), video.getContent());
+                    File thumbnail = videoContentProcessor.generateThumbnail(file);
+                    video.setThumbnail(FileUtils.readFileToByteArray(thumbnail));
+                    log.info("Generated thumbnail for the id={}", id);
+                    deleteQuietly(file, thumbnail);
+                    videoRepository.save(video);
+                    log.info("Saved thumbnail for the video id={}", id);
+                    successList.add(id);
+                } catch (Exception e) {
                     log.error("Unable to save the file with id {}", id);
                     failureList.add(id);
                 }
@@ -92,4 +115,15 @@ public class ContentService {
                 .failureList(failureList)
                 .build();
     }
+
+    private void deleteQuietly(File... files) {
+        for (File file : files) {
+            FileUtils.deleteQuietly(file);
+        }
+    }
+
+    private String getFileName(String sessionId, Long id, String extn) {
+        return MessageFormat.format("{0}_{1}.{2}", sessionId, id, extn);
+    }
+
 }
